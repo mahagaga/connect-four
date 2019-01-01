a8997441bcd151d71929ed9de98cb5b3f5b5d043
schmide <emanuel.schmid@id.ethz.ch>
2019-01-02 00:21:48 +0100

    implementation of move evaluation

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@ mod tests {
         let mut cf = ConnectFour::new();
         let p = Player::Black;
         let pm = cf.possible_moves(&p);
-        assert!(pm.len()==cf.width);
+        assert!(pm.len()==ConnectFour::width());
         assert!(*pm[3].data() == Column::Four);
 
         for _ in 0..6 {
@@ -70,7 +70,7 @@ mod tests {
         for x in &pm {
             println!("{:?}", &x.data());
         }
-        assert!(pm.len()==cf.width-1);
+        assert!(pm.len()==ConnectFour::width()-1);
         assert!(*pm[3].data() == Column::Five);
     }
 
@@ -78,7 +78,11 @@ mod tests {
     fn test_find_best_move() {
         let white = Player::White;
         let black = Player::Black;
-        let strategy = ConnectFourStrategy {};
+        let strategy = ConnectFourStrategy {
+             mscore_koeff: 1.0,
+             oscore_koeff: 0.8,
+             nscore_koeff: 0.5
+        };
 
         // recognize a winner
         let mut game = ConnectFour::new();
@@ -119,13 +123,40 @@ mod tests {
         game = ConnectFour::new();
         if let (Some(mv), Some(score)) = strategy.find_best_move(
                 Rc::new(RefCell::new(game)), &white, 2, true) {
-            println!("{:?} {:?}", mv.data(), score);
+            println!("{:?} {:?} vs {}", mv.data(), score,
+                              12 as f32 * strategy.mscore_koeff * strategy.nscore_koeff
+                            + 12 as f32 * strategy.oscore_koeff * strategy.nscore_koeff);
             match score {
-                    Score::Undecided(p) => assert!(p==0.5),
+                    Score::Undecided(ev) => assert!(
+                        ev == 12 as f32 * strategy.mscore_koeff * strategy.nscore_koeff
+                            + 12 as f32 * strategy.oscore_koeff * strategy.nscore_koeff),
                     _ => assert!(false),
             }
             assert!(*mv.data() == Column::Four);
         } else { assert!(false); }
+    }
+
+    #[test]
+    fn test_evaluate_move() {
+        let mut g = ConnectFour::new();
+        let black = Player::Black;
+        let white = Player::White;
+        let _ = g.drop(&black, Column::Two);
+        let mv = ConnectFourMove { data: Column::Five, };
+        let s = ConnectFourStrategy  {
+             mscore_koeff: 1.0,
+             oscore_koeff: 0.8,
+             nscore_koeff: 0.5,
+        };
+
+        let expected = 7 as f32 * s.mscore_koeff * s.nscore_koeff
+                     + 0 as f32 * s.mscore_koeff
+                     + 7 as f32 * s.oscore_koeff * s.nscore_koeff
+                     + 1 as f32 * s.oscore_koeff;
+        if let Ok(eval) = s.evaluate_move(Rc::new(RefCell::new(g)), &white, Rc::new(mv)) {
+            println!("expected score {} vs calculated {}", expected, eval);
+            assert!(eval == expected)
+        } else { assert!(false) }
     }
 }
 
@@ -143,7 +174,7 @@ pub enum Player {
 }
 
 impl Player {
-    fn oponent(&self) -> &Player {
+    fn opponent(&self) -> &Player {
         match self {
             Player::Black => &Player::White,
             Player::White => &Player::Black,
@@ -178,23 +209,26 @@ pub enum Withdraw {
     NotAllowed,
 }
 
-pub trait Game<T> {
+pub trait Game<T,S> {
     fn possible_moves(&self, p: &Player) -> Vec<Rc<Move<T>>>;
     fn make_move(&mut self, p: &Player, m: Rc<Move<T>>) -> Result<Score, Withdraw>;
     fn withdraw_move(&mut self, p: &Player, m: Rc<Move<T>>);
     fn display(&self) -> String;
-    fn evaluate_move(&self, p: &Player, m: Rc<Move<T>>) -> Result<f32, Withdraw>;
+    fn state(&self) -> &S;
 }
 
 //### strategy ####################################################################################
 
-pub trait Strategy<T> {
+pub trait Strategy<T,S> {
+    fn evaluate_move(&self, g: Rc<RefCell<Game<T,S>>>, p: &Player, m: Rc<Move<T>>) -> Result<f32, Withdraw>;
+
     fn find_best_move(&self, 
-            g: Rc<RefCell<Game<T>>>,
+            g: Rc<RefCell<Game<T,S>>>,
             p: &Player,
             moves_ahead: i32,
             game_evaluation: bool
         ) -> (Option<Rc<Move<T>>>, Option<Score>) {
+
         //let mut win_option: Option<Rc<Move<T>>> = None;
         let mut remis_option: Option<Rc<Move<T>>> = None;
         let mut lost_option: Option<Rc<Move<T>>> = None;
@@ -224,7 +258,7 @@ pub trait Strategy<T> {
         for (undecided, pv) in follow_ups {
             if moves_ahead > 0 {
                 let _ = g.borrow_mut().make_move(p, Rc::clone(&undecided));
-                let (_, advscore) = self.find_best_move(Rc::clone(&g), p.oponent(), moves_ahead-1, false);
+                let (_, advscore) = self.find_best_move(Rc::clone(&g), p.opponent(), moves_ahead-1, false);
                 match advscore {
                     Some(Score::Won) => { lost_option = Some(Rc::clone(&undecided)); },
                     Some(Score::Remis) => { remis_option = Some(Rc::clone(&undecided)); },
@@ -247,7 +281,7 @@ pub trait Strategy<T> {
         let mut undecided_pv = -1.0;
         for (undecided, pv) in still_undecided {
             if game_evaluation {
-                match g.borrow().evaluate_move(p, Rc::clone(&undecided)) {
+                match self.evaluate_move(Rc::clone(&g), p, Rc::clone(&undecided)) {
                     Ok(ev) => if ev > undecided_pv {
                         if ev > undecided_pv {
                             undecided_option = Some(undecided);
@@ -286,8 +320,6 @@ pub trait Strategy<T> {
 
 pub struct ConnectFour {
     field: Vec<Vec<Option<Player>>>,
-    width: usize,
-    hight: usize,
 }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -339,12 +371,12 @@ impl Move<Column> for ConnectFourMove {
     }
 }
 
-impl Game<Column> for ConnectFour {
+impl Game<Column,Vec<Vec<Option<Player>>>> for ConnectFour {
     fn possible_moves(&self, _: &Player) -> Vec<Rc<Move<Column>>> {
         let mut allowed: Vec<Rc<Move<Column>>> = Vec::new();
         let mut i:usize = 0;
         for col in &self.field {
-            if col.len() < self.hight {
+            if col.len() < ConnectFour::height() {
                 allowed.push(Rc::new(ConnectFourMove {
                     data: Column::from_usize(i)
                 }));
@@ -356,7 +388,7 @@ impl Game<Column> for ConnectFour {
 
     fn make_move(&mut self, p: &Player, mv: Rc<Move<Column>>) -> Result<Score, Withdraw> {
         let n = mv.data().to_usize();
-        if self.hight == self.field[n].len() {
+        if ConnectFour::height() == self.field[n].len() {
             // column is obviously already filled to the top
             Err(Withdraw::NotAllowed)
         } else {
@@ -371,10 +403,6 @@ impl Game<Column> for ConnectFour {
     fn withdraw_move(&mut self, _p: &Player, mv: Rc<Move<Column>>) {
         let n = mv.data().to_usize();
         self.field[n].pop();
-    }
-
-    fn evaluate_move(&self, p: &Player, mv: Rc<Move<Column>>) -> Result<f32, Withdraw> {
-        Ok(0.0)
     }
 
     fn display(&self) -> String {
@@ -394,6 +422,9 @@ impl Game<Column> for ConnectFour {
         s.push_str("------");
         s
     }
+    fn state(&self) -> &Vec<Vec<Option<Player>>> {
+        &self.field
+    }
 }
 
 enum Step {
@@ -405,14 +436,15 @@ enum Step {
 //### connect four ################################################################################
 
 impl ConnectFour {
+    fn width() -> usize { 7 }
+    fn height() -> usize { 6 }
+    
     pub fn new() -> Self {
         let mut cf = ConnectFour{
-            width: 7,
-            hight: 6,
             field: Vec::with_capacity(7),
         };
-        for _coln in 0..cf.width {
-            let mut col:Vec<Option<Player>> = Vec::with_capacity(cf.hight);
+        for _coln in 0..ConnectFour::width() {
+            let mut col:Vec<Option<Player>> = Vec::with_capacity(ConnectFour::height());
             cf.field.push(col);
         };
         cf
@@ -500,7 +532,148 @@ impl ConnectFour {
 }
 
 pub struct ConnectFourStrategy {
+    oscore_koeff: f32,
+    mscore_koeff: f32,
+    nscore_koeff: f32,
+}
+enum Cell {
+    M, //my stone
+    O, //opponent's stone
+    N, //no stone, empty cell
 }
 
-impl Strategy<Column> for ConnectFourStrategy {
+impl ConnectFourStrategy {
+    fn display_efield(&self, ef: &Vec<Vec<Cell>>) {
+        for j in (0..ConnectFour::height()).rev() {
+            for i in 0..ConnectFour::width() {
+                print!("{}", match ef[i][j] {
+                    Cell::N => ".",
+                    Cell::M => "x",
+                    Cell::O => "o",
+                })
+            }
+            println!("|")
+        }
+    }
+
+    fn efield_counting(&self, ef: &Vec<Vec<Cell>>, ns: Vec<usize>, ms: Vec<usize>)
+    -> ((i32, i32, i32,), (i32, i32, i32)) {
+        let mut o_count = 0;
+        let mut no_count = 0;
+        let mut m_count = 0;
+        let mut nm_count = 0;
+        let mut count = 0;
+        let mut first_opponent:Option<i32> = None;
+        let mut first_mine:Option<i32> = None;
+        for (i,j) in ns.iter().zip(ms.iter()) {
+            match ef[*i][*j] {
+                Cell::M => { if let None = first_mine { first_mine = Some(count); }
+                             if let None = first_opponent { m_count += 1; }},
+                Cell::O => { if let None = first_opponent { first_opponent = Some(count); }
+                             if let None = first_mine { o_count += 1; }},
+                Cell::N => { if let None = first_mine { no_count += 1; }
+                             if let None = first_opponent { nm_count += 1; }},
+            }
+            count += 1;
+        }
+        if let None = first_mine { first_mine = Some(count); }
+        if let None = first_opponent { first_opponent = Some(count); }
+        match first_mine {
+            Some(fm) => match first_opponent {
+                Some(fo) => ((fo,m_count,nm_count),(fm,o_count,no_count)),
+                _ => ((0,0,0),(0,0,0))
+            }
+            _ => ((0,0,0),(0,0,0))
+        }
+    }
+}
+
+use std::cmp;
+impl Strategy<Column,Vec<Vec<Option<Player>>>> for ConnectFourStrategy {
+    
+    fn evaluate_move(&self, g: Rc<RefCell<Game<Column,Vec<Vec<Option<Player>>>>>>, p: &Player, mv: Rc<Move<Column>>) 
+    -> Result<f32, Withdraw> {
+        let n = mv.data().to_usize();
+        let m = g.borrow().state()[n].len();
+
+        // fill evaluation field with empty cells
+        let mut efield = Vec::with_capacity(ConnectFour::width());
+        for _ in 0..ConnectFour::width() {
+            let mut ecol = Vec::with_capacity(ConnectFour::height());
+            for _ in 0..ConnectFour::height() {
+                ecol.push(Cell::N);
+            }
+            efield.push(ecol);
+        }
+
+        // copy current state into evaluation field
+        let black = |player: &Player| { match player { Player::Black => Cell::M, Player::White => Cell::O }};
+        let white = |player: &Player| { match player { Player::White => Cell::M, Player::Black => Cell::O }};
+        let mut i:usize = 0;
+        for c in  g.borrow().state() { // that's the current Connect Four field
+            let mut j:usize = 0;
+            for f in c {
+                match f {
+                    Some(Player::Black) => efield[i][j] = black(p),
+                    Some(Player::White) => efield[i][j] = white(p),
+                    None => (),
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+        
+        let mut total_score = 0.0;
+        let score_arithmetics = |((mfree_left, m_left, nm_left), (ofree_left, o_left, no_left)), 
+                                 ((mfree_right,m_right,nm_right),(ofree_right,o_right,no_right))| -> f32 {
+            let mut partial_score = 0.0;
+            if mfree_left + mfree_right >= 3 {
+                partial_score += self.mscore_koeff * (m_left + m_right) as f32;
+                partial_score += self.mscore_koeff * self.nscore_koeff * (nm_left + nm_right) as f32;
+            }
+            if ofree_left + ofree_right >= 3 {
+                partial_score += self.oscore_koeff * (o_left + o_right) as f32;
+                partial_score += self.oscore_koeff * self.nscore_koeff * (no_left + no_right) as f32;
+            }
+            partial_score
+        };
+
+        // horizontal score
+        let show:Vec<usize> = (match n { s if s < 3 => 0, b => b-3 }..n).rev().collect();
+        println!("{:?} {} {}", show, match n { s if s < 3 => 0, b => b-3 }, n);
+        let ontheleft = self.efield_counting(&efield,
+            (match n { s if s < 3 => 0, b => b-3 }..n).rev().collect(),
+            vec!(m,m,m));
+        let ontheright = self.efield_counting(&efield,
+            (cmp::min(ConnectFour::width(), n+1)..cmp::min(ConnectFour::width(), n+4)).collect(),
+            vec!(m,m,m));
+        total_score += score_arithmetics(ontheleft, ontheright);
+
+        // diagonal score '/'
+        let ontheleft = self.efield_counting(&efield,
+            (match n { s if s < 3 => 0, b => b-3 }..n).rev().collect(),
+            (match m { s if s < 3 => 0, b => b-3 }..m).rev().collect());
+        let ontheright = self.efield_counting(&efield,
+            (cmp::min(ConnectFour::width(), n+1)..cmp::min(ConnectFour::width(), n+4)).collect(),
+            (cmp::min(ConnectFour::height(),m+1)..cmp::min(ConnectFour::height(),m+4)).collect());
+        total_score += score_arithmetics(ontheleft, ontheright);
+
+        // diagonal score '\'
+        let ontheleft = self.efield_counting(&efield,
+            (match n { s if s < 3 => 0, b => b-3 }..n).rev().collect(),
+            (cmp::min(ConnectFour::height(),m+1)..cmp::min(ConnectFour::height(),m+4)).collect());
+        let ontheright = self.efield_counting(&efield,
+            (cmp::min(ConnectFour::width(), n+1)..cmp::min(ConnectFour::width(), n+4)).collect(),
+            (match m { s if s < 3 => 0, b => b-3 }..m).rev().collect());
+        total_score += score_arithmetics(ontheleft, ontheright);
+
+        // vertical score
+        let ((_, m_below, _),(_, o_below, _)) = self.efield_counting(&efield,
+            vec!(n,n,n),
+            (match m { s if s < 3 => 0, b => b-3 }..m).rev().collect());
+        total_score += self.mscore_koeff * m_below as f32;
+        total_score += self.oscore_koeff * o_below as f32;
+        
+        Ok(total_score)
+    }
 }
